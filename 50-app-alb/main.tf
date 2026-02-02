36a990dd6b43b66f2baa21664f419998959064cb
srinivasdevops97 <birudurajusrinivas@gmail.com>
2026-02-02 19:58:35 +0530

    terraform

--- 50-app-alb/main.tf
+++ 50-app-alb/main.tf
@@ -1,12 +1,12 @@
 module "app_alb" {
   source = "terraform-aws-modules/alb/aws"
-  enable_deletion_protection = false
 
   internal = true
   name    = "${local.resource_name}-app-alb"  #expense-dev-app-alb
   vpc_id  = local.vpc_id
   subnets = local.private_subnet_ids
   create_security_group = false
+  enable_deletion_protection = false
 
   # Security Group
   security_groups = [data.aws_ssm_parameter.app_alb_sg_id.value]
