d6f9a759aea609c7fa79e5ee18d3c63bf3602068
srinivasdevops97 <birudurajusrinivas@gmail.com>
2026-02-02 19:10:29 +0530

    terraform

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-component = $1
-environment = $2
-component="${var.component}"
-environment="${var.environment}"
+component=$1
+environment=$2
 echo "component: $component, environment: $environment"
 dnf install ansible -y
-ansible-pull -i localhost, -U https://github.com/srinivasdevops97/expense-ansible-roles-tf.git main.yaml -e component="${var.component}" -e environment="${var.environment}"
+ansible-pull -i localhost, -U https://github.com/srinivasdevops97/expense-ansible-roles-tf.git main.yaml -e component=$component -e environment=$environment
